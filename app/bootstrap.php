2361456b09a260ea8e18dadd60930715499e88c8
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-08-10 17:04:20 -0300

    Estandendo templates

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -25,4 +25,8 @@ $container['HomeController'] = function ($container) {
     return new App\Controllers\HomeController($container);
 };
 
+$container['AuthController'] = function ($container) {
+    return new App\Controllers\AuthController($container);
+};
+
 require __DIR__ . '/routes.php';
--- app/routes.php
+++ app/routes.php
@@ -5,4 +5,7 @@
 //     return 'Hello, World!';
 // });
 
-$app->get('/', 'HomeController:index');
+$app->get('/', 'HomeController:index')->setName('home');
+
+$app->get('/login', 'AuthController:login')->setName('auth.login');
+$app->get('/registrar', 'AuthController:register')->setName('auth.register');
